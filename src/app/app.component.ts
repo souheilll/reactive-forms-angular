bf0b043732b674e0411c9487c951f6f2a7826d47
souheil <souheil.mansouri@lookiimobile.com>
2023-05-10 11:23:12 +0100

    add formArray

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -18,14 +23,32 @@ export class AppComponent implements OnInit {
     password: ['', Validators.required],
     email: ['', [Validators.required, Validators.email]],
     roleId: [1, Validators.required],
+    // aliases: new FormArray([]),
+    aliases: this.fb.array([]),
   });
   constructor(private fb: FormBuilder) {}
+
   ngOnInit(): void {
     // this method allowing to react to specific without submitting the whole form
     this.registerForm.get('roleId')?.valueChanges.subscribe((roleId) => {
       console.log('send a request and update the roleId', roleId);
     });
   }
+
+  get aliases() {
+    return this.registerForm.get('aliases') as FormArray;
+  }
+
+  // this method allowing to add new input
+  addAlias() {
+    console.log(this.aliases);
+    const controls = this.fb.group({
+      name: ['', Validators.required],
+      amount: ['', Validators.required],
+    });
+    this.aliases.push(controls);
+  }
+
   onSubmit() {
     console.log('submittedForm', this.registerForm.value);
     console.log('form submitted');
